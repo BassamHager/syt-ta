d1c7ce21b31a9a6d9160b725a60622ab1ca8975e
BassamHajar <b2dndr1@gmail.com>
2022-06-23 14:05:40 +0200

    updated responsiveness on different screen sizes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 // components
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 import YachtGallery from "./components/yachts/YachtGallery";
-// lightbox library
 
 const App = () => (
   <div className="app">
+    <Header />
     <div className="app-overlay">
       <div className="page-content">
         {/* loading... */}
@@ -12,6 +14,7 @@ const App = () => (
         <YachtGallery />
       </div>
     </div>
+    <Footer />
   </div>
 );
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.scss";
+
+const Header = () => (
+  <div className="header-overlay">
+    <h1 className="app-header">SuperYacht Times</h1>
+  </div>
+);
+
+export default Header;
